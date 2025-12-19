23432d2c2cbf53951dd08ebb000c2fb1a13a7c7d
IliasSari <sarigilias@gmail.com>
2025-12-19 23:09:04 +0200

    Second commit

--- MyFrame.java
+++ MyFrame.java
@@ -1,17 +1,21 @@
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
-public class MyFrame extends JFrame implements ActionListener{
+public class MyFrame extends JFrame {
 
     JButton button;
 
     MyFrame(){
 
         JButton button = new JButton();
-        button.setBounds(200, 100, 100, 50);
-
+        button.setBounds(200, 100, 150, 50);
+        button.addActionListener(e -> System.out.println("Hello"));
+        button.setText("This is a button");
+        button.setFocusable(false);
+        button.setHorizontalTextPosition(JButton.CENTER);
+        button.setVerticalTextPosition(JButton.BOTTOM);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setLayout(null);
         this.setSize(500, 500);
@@ -20,7 +24,7 @@ public class MyFrame extends JFrame implements ActionListener{
     }
     public void actionPerformed(ActionEvent e){
         if(e.getSource()==button){
-            System.out.println("poo");
+            System.out.println("Hello");
         }
 
     }
